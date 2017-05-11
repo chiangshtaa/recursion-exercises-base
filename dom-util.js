d8a5c23621c1382194d5bf5258c154cddc8c5340
Michael Chiang <chiangshtaa@gmail.com>
2017-05-10 19:50:39 -0700

    add dom-util implementation

--- dom-util.js
+++ dom-util.js
@@ -2,11 +2,19 @@
 const visitAllNodes = function(node, callback) {
   // Hint: read about DOM nodes and node methods here: https://developer.mozilla.org/en-US/docs/Web/API/Node
   // Your code here
+  if (node.childNodes.length === 0) {
+    return callback(node);
+  }
+  callback(node);
+  node.childNodes.forEach((node) => visitAllNodes(node, callback));
 };
-
+ 
 const flattenTreeToArray = function(node) {
   // Hint: Use visitAllNodes()
   // Your code here
+  const arr = [];
+  visitAllNodes(node, (node) => arr.push(node));
+  return arr;
 };
 
 module.exports = {
