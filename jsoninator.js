26c2b74e550219353e0b450bb2043163d0f0b7b3
Michael Chiang <chiangshtaa@gmail.com>
2017-05-10 23:00:40 -0700

    add jsoninator implementation

--- jsoninator.js
+++ jsoninator.js
@@ -8,6 +8,27 @@ const _ = require('underscore'); // the real one! :)
 
 const stringify = function(obj) {
   // your code goes here
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof(obj) === 'number') {
+    return obj.toString();
+  } else if (typeof(obj) === 'boolean') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    const results = obj.map((element) => {
+      return stringify(element);
+    });
+    return '[' + results.join(',') + ']';
+  } else {
+    let result = '{';
+    for (let key in obj) {
+      result += stringify(key) + ':' + stringify(obj[key]);
+    }
+    result += '}';
+    return result;
+  }
 };
 
 module.exports = {
