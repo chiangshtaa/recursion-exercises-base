903bfaaa198b64b42a26d726756aca9cbcae9cfa
Michael Chiang <chiangshtaa@gmail.com>
2017-05-10 21:06:21 -0700

    add dominator implementation

--- dominator.js
+++ dominator.js
@@ -3,14 +3,38 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   // Your code here
+  const elements = flattenTreeToArray(root);
+  let result;
+  elements.forEach((el) => {
+    if (el.id === id) {
+      result = el;
+    }
+  });
+  return result;
 };
 
 const getElementsByClassName = function(root, className) {
   // Your code here
+  const elements = flattenTreeToArray(root);
+  let result = [];
+  elements.forEach((el) => {
+    if ((el.className !== undefined) && (el.className.indexOf(className) !== -1)) {
+      result.push(el);
+    }
+  });
+  return result;
 };
 
 const getElementsByTagName = function(root, tagName) {
   // Your code here
+  const elements = flattenTreeToArray(root);
+  let result = [];
+  elements.forEach((el) => {
+    if (el.tagName === tagName) {
+      result.push(el);
+    }
+  });
+  return result;
 };
 
 module.exports = {
